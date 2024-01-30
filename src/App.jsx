db41b83516f3f761a760708f229a764935d14745
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-01-30 09:51:34 +0000

    template done

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "./ui/card"
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      <Button>Hello World</Button>
+      {/* <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card> */}
+
+    </div>
+    </>
+  )
+}
+
+export default App
